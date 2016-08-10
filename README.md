69f3977c4bcc24b64fa502cddf7fcacead2056e8
Carina Carriedo <carriedo.carina@gmail.com>
2016-08-10 11:04:33 -0700

    moved middlwareware and added heroku process

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 //
 What is middleware? How does it get used? How do you check it?
 How do you add custom middlware to routes?
+
+Why do we move code and export and require?
--- server.js
+++ server.js
@@ -1,21 +1,11 @@
 var express = require('express')
 var app = express(); // creating an express app
 var path = require('path');
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
+var middleware = require('./middleware')
 
-//Custom middleware
-var middleware = {
-  requireAuthentication: function(req, res, next){
-  console.log("Request Auth Ran.");
-  next();
-},
-logger: function(req, res, next){
-  console.log(req.method + req.originalUrl + new Date().toString())
-  next();
-  }
-}
 
-app.use(middleware.requireAuthentication); // This middlware is now for the whole app
+//app.use(middleware.requireAuthentication); // This middlware is now for the whole app
 
 app.get('/', function(req, res){
   res.send('<h1>Express Todo API </h1>')
